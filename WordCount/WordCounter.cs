a69d0fb22cc22d0037c9f9c95c0595898342458c
Yan-Yi Li <liyanyi@gmail.com>
2016-06-16 21:55:15 +0100

    Initial commit
    - Project setup with NUnit

---
+++ WordCount/WordCounter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordCount
+{
+	public class WordCounter
+	{
+		public WordCounter()
+		{
+		}
+	}
+}
+
---
+++ WordCount/WordCounterTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace WordCount
+{
+	[TestFixture]
+	public class WordCounterTest
+	{
+		[Test]
+		public void TestCase()
+		{
+		}
+	}
+}
+
