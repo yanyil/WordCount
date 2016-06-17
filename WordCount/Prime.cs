598f202443d9c25ea05f92b2e78858f45138787c
Yan-Yi Li <liyanyi@gmail.com>
2016-06-17 17:58:41 +0100

    Add Prime Class
    - Add IsPrime method to check whether a number is prime
    - Add test case for negative numbers

---
+++ WordCount/Prime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WordCount
+{
+    public static class Prime
+    {
+        public static bool IsPrime(int number)
+        {
+            return false;
+        }
+    }
+}
+
---
+++ WordCountTest/PrimeTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace WordCount
+{
+    [TestFixture]
+    public class PrimeTest
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(-2)]
+        [TestCase(-3)]
+        [TestCase(-5)]
+        [TestCase(-6)]
+        public void Negatives(int number)
+        {
+            Assert.False(Prime.IsPrime(number));
+        }
+    }
+}
