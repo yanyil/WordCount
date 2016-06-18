acb4b55e32d5da99208845cf5f80b85eeb8fcfd6
Yan-Yi Li <liyanyi@gmail.com>
2016-06-18 06:56:22 +0100

    Add BookInfo Class
    - Display word counts to console
    - Take path of file to be read as argument in Main
    - Embed test txt file for testing

---
+++ WordCount/BookInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace WordCount
+{
+    public class BookInfo
+    {
+        public static void Main(string[] args)
+        {
+            var text = File.ReadAllText(args[0]);
+
+            foreach(var word in WordCounter.CountWords(text))
+            {
+                Console.WriteLine("{0} {1}", word.Key, word.Value);
+            }
+        }
+    }
+}
---
+++ WordCountTest/BookInfoTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace WordCount
+{
+    [TestFixture]
+    public class BookInfoTest
+    {
+        [Test]
+        public void Output_Book_Info()
+        {
+            TextWriter originalConsole = Console.Out;
+
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+
+                var baseDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var filePath = baseDir + "/TestData/test.txt";
+
+                BookInfo.Main(new string[] { filePath });
+
+                var expected = "a 1\n" +
+                               "text 1\n" +
+                               "file 1\n" +
+                               "for 1\n" +
+                               "testing 2" +
+                               Environment.NewLine;
+
+                Assert.That(writer.ToString(), Is.EqualTo(expected));
+            }
+
+            Console.SetOut(originalConsole);
+        }
+    }
+}
